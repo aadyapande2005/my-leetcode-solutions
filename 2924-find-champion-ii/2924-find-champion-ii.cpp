af83201259e09ba0765b1ea56d276f63b5ee5de9
Aadya Pande <pandeaadya2005@gmail.com>
2026-03-30 23:43:14 +0530

    Time: 45 ms (21.55%), Space: 104.8 MB (25.62%) - LeetHub

---
+++ 2924-find-champion-ii/2924-find-champion-ii.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int findChampion(int n, vector<vector<int>>& edges) {
+        vector<int> indeg(n);
+
+        for(auto edge:edges)
+        {
+            indeg[edge[1]]++;
+        }
+
+        bool flag = false;
+        int ans = -1;
+
+        for(int i=0; i<n; i++)
+        {
+            if(indeg[i] == 0)
+            {
+                if(flag) return -1;
+                flag = true;
+                ans = i;
+            }
+        }
+
+        return ans;
+    }
+};
