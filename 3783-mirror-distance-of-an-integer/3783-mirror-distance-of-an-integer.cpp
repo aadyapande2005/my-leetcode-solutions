5d6cd1bb60decd2aec17e59cd8b64ce3ba9f3d18
Aadya Pande <pandeaadya2005@gmail.com>
2026-04-18 13:31:52 +0530

    Time: 0 ms (100%), Space: 9.9 MB (8.47%) - LeetHub

--- 3783-mirror-distance-of-an-integer/3783-mirror-distance-of-an-integer.cpp
+++ 3783-mirror-distance-of-an-integer/3783-mirror-distance-of-an-integer.cpp
@@ -1,11 +1,53 @@
 class Solution {
 public:
+    int rev(int n)
+    {
+        /*
+            n = 2541
+            2541/10 = 254
+            2541%10 = 1
+
+            254/10 = 25
+            254%10 = 4
+
+            25/10 = 2
+            25%10 = 5
+
+            2/10 = 0
+            2%10 = 2
+
+            stack = 2 5 4 1
+
+            2 + 50 + 400 + 1000
+        */
+        stack<int> st;
+
+        while(n > 0)
+        {
+            int num = n/10;
+            int rem = n%10;
+
+            st.push(rem);
+            n = num;
+        }
+
+        int pw = st.size();
+
+        int rev_num = 0;
+
+        for(int i=0; i<pw; i++)
+        {
+            rev_num += st.top()*pow(10,i);
+            st.pop();
+        }
+
+        return rev_num;        
+    }
     int mirrorDistance(int n) {
         ios::sync_with_stdio(false);
         cin.tie(nullptr);
         
-        string temp = to_string(n);
-        reverse(temp.begin(), temp.end());
-        return abs(stoi(temp) - n);
+        int rev_num = rev(n);
+        return abs(rev_num - n);
     }
 };
