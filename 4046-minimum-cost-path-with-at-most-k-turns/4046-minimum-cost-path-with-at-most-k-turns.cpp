049d5c82e305b50c78727943cd62f65261945f1e
Aadya Pande <pandeaadya2005@gmail.com>
2026-09-09 22:47:54 +0530

    Time: 427 ms (84.03%), Space: 122.2 MB (35.77%) - LeetHub

--- 4046-minimum-cost-path-with-at-most-k-turns/4046-minimum-cost-path-with-at-most-k-turns.cpp
+++ 4046-minimum-cost-path-with-at-most-k-turns/4046-minimum-cost-path-with-at-most-k-turns.cpp
@@ -1,24 +1,20 @@
 class Solution {
 public:
     int n, m;
-    vector<vector<vector<int>>> dp;
+    vector<vector<vector<vector<int>>>> dp;
     vector<vector<int>> grid;
     
-    int sol(int ind, int direction, int k)
+    int sol(int i, int j, int direction, int k)
     {
-        int i = ind / m;
-        int j = ind % m; 
-
         // cout<<i<<" "<<j<<endl;
-
         if(k < 0) return 1e9;
         if(i == n-1 && j == m-1) return grid[i][j];
-        if(dp[ind][k][direction] != -1) return dp[ind][k][direction];
+        if(dp[i][j][k][direction] != -1) return dp[i][j][k][direction];
 
 
         int dx[] = {-1, 0, 1, 0};
         int dy[] = {0, 1, 0, -1};
-        int dir[] = {0,1,2,3};
+        int dir[] = {0, 1, 2, 3};
 
         int res = 1e9;
 
@@ -30,25 +26,32 @@ public:
 
             if(x>=0 && y>=0 && x<n && y<m)
             {
-                int new_ind = x*m + y;
-
-                if(direction == 5 || d == direction) 
-                    res = min(res, sol(new_ind, d, k));
+                if(direction == 4 || d == direction) 
+                    res = min(res, sol(x, y, d, k));
                 else 
-                    res = min(res, sol(new_ind, d, k-1));
+                    res = min(res, sol(x, y, d, k-1));
             }
         }
-        return dp[ind][k][direction] = grid[i][j] + res;
+        return dp[i][j][k][direction] = grid[i][j] + res;
     }
+
     int minCost(vector<vector<int>>& grid, int k) {
         n = grid.size();
         m = grid[0].size();
 
-        dp.resize(n*m, vector<vector<int>>(k+1, vector<int>(6, -1)));
+        dp.resize(
+            n, vector<vector<vector<int>>>(
+                m, vector<vector<int>>(
+                    k+1, vector<int>(
+                        5, -1
+                    )
+                )
+            )            
+        );
 
         this->grid = grid;        
 
-        int ans = sol(0,5,k); 
+        int ans = sol(0, 0, 4, k); 
 
         return ans >= 1e9 ? -1 : ans;
     }
