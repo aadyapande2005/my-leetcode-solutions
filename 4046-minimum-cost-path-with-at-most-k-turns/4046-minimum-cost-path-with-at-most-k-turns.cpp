0b641a82d0a10465265df0898cacb589ac0f854a
Aadya Pande <pandeaadya2005@gmail.com>
2026-09-09 20:36:42 +0530

    Time: 397 ms (87.73%), Space: 118.5 MB (39.24%) - LeetHub

---
+++ 4046-minimum-cost-path-with-at-most-k-turns/4046-minimum-cost-path-with-at-most-k-turns.cpp
@@ -0,0 +1,55 @@
+class Solution {
+public:
+    int n, m;
+    vector<vector<vector<int>>> dp;
+    vector<vector<int>> grid;
+    
+    int sol(int ind, int direction, int k)
+    {
+        int i = ind / m;
+        int j = ind % m; 
+
+        // cout<<i<<" "<<j<<endl;
+
+        if(k < 0) return 1e9;
+        if(i == n-1 && j == m-1) return grid[i][j];
+        if(dp[ind][k][direction] != -1) return dp[ind][k][direction];
+
+
+        int dx[] = {-1, 0, 1, 0};
+        int dy[] = {0, 1, 0, -1};
+        int dir[] = {0,1,2,3};
+
+        int res = 1e9;
+
+        for(int a=0; a<4; a++)
+        {
+            int x = i + dx[a];
+            int y = j + dy[a];         
+            int d = dir[a];
+
+            if(x>=0 && y>=0 && x<n && y<m)
+            {
+                int new_ind = x*m + y;
+
+                if(direction == 5 || d == direction) 
+                    res = min(res, sol(new_ind, d, k));
+                else 
+                    res = min(res, sol(new_ind, d, k-1));
+            }
+        }
+        return dp[ind][k][direction] = grid[i][j] + res;
+    }
+    int minCost(vector<vector<int>>& grid, int k) {
+        n = grid.size();
+        m = grid[0].size();
+
+        dp.resize(n*m, vector<vector<int>>(k+1, vector<int>(6, -1)));
+
+        this->grid = grid;        
+
+        int ans = sol(0,5,k); 
+
+        return ans >= 1e9 ? -1 : ans;
+    }
+};
